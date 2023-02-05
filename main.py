b109a518ab511dc5b056f750fd792d3f07a0c446
Michał Nedza <MicnedSQ@gmail.com>
2023-02-05 14:35:01 +0100

    update

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from pytube import Playlist
+import os
 
 url = input("Enter Playlist url: ")
 
 yt_playlist = Playlist(url)
+
+os.mkdir(r"C:\Users\Micne\Desktop\YouTube_downloader\Downloads")
 
 for video in yt_playlist.videos:
     video.streams.get_highest_resolution().download(r"C:\Users\Micne\Desktop\YouTube_downloader\Downloads")
